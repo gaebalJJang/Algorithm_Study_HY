1ac861a0279900c82d398c502c1c8206c0a2a186
HeeyeonCho <78330890+HeeyeonCho@users.noreply.github.com>
2021-07-18 21:10:22 +0900

    바둑이 승차

--- Section06/5_바둑이 승차.py
+++ Section06/5_바둑이 승차.py
@@ -1,5 +1,6 @@
 import sys
 #sys.stdin=open("input.txt", "r")
+
 def DFS(L, sum, tsum):
     global result
     if sum+(total-tsum)<result:
@@ -12,7 +13,6 @@ def DFS(L, sum, tsum):
     else:
         DFS(L+1, sum+a[L], tsum+a[L])
         DFS(L+1, sum, tsum+a[L])
-
 
 if __name__=="__main__":
     c, n=map(int, input().split())
